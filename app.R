c4395e8c2bf6468b030d2646537c23b5f0bca993
Sophie <75918453+Sop51@users.noreply.github.com>
2023-11-24 17:44:43 -0500

    filter counts

--- app.R
+++ app.R
@@ -10,15 +10,18 @@ library(ggplot2)
 library(colourpicker) 
 library(dplyr)
 library(tidyr)
-library(gridExtra)
-library(png)
+library(DESeq2)
 
-# define the ui for the application
+# increase the shiny upload size option for the counts data
+options(shiny.maxRequestSize=30*1024^2) 
+
+# UI FOR THE APPLICATION DEFINED HERE #
 ui <- fluidPage(
   tabsetPanel(
     sidebarLayout(
       sidebarPanel(
-        fileInput(inputId = "uploadedSampleFile", label = "Upload a CSV file containing sample information:")
+        fileInput(inputId = "uploadedSampleFile", label = "Upload a file containing sample information:"),
+        fileInput(inputId = "uploadedCountsFile", label = "Upload a file containing counts information:")
       ),
       mainPanel(
         tabsetPanel(
@@ -37,7 +40,16 @@ ui <- fluidPage(
                               plotOutput("sampleHistogram"))
                    )
           ),
-          tabPanel("Counts", "content"),
+          tabPanel("Counts",
+                   tabsetPanel(
+                     tabPanel("Filter Information",
+                              sliderInput("varianceSlider", "Variance Percentile Threshold", min = 0, max = 100, value = 50),
+                              sliderInput("nonzeroSlider", "Min Non-Zero Samples", min = 0, max = 5000, value = 10),
+                              tableOutput("countsFilteredTable")),
+                     tabPanel("Diagnostic Plot"),
+                     tabPanel("Heatmap"),
+                     tabPanel("PCA")
+                   )),
           tabPanel("DE", "content")
         )
       )
@@ -46,7 +58,7 @@ ui <- fluidPage(
 )
 
 
-# Define server logic required
+# SERVER LOGIC DEFINED HERE #
 server <- function(input, output, session) {
   
   ### Define a function that reads in the sample data ###
@@ -62,6 +74,43 @@ server <- function(input, output, session) {
       as.numeric(gsub(",", "", as.character(x)))
     })
     return(df)
+  })
+  
+  ### Define a function that reads in the counts data + normalizes ###
+  load_count_data <- reactive({
+    req(input$uploadedSampleFile)
+    # read in the sample data
+    df <- read.table(input$uploadedCountsFile$datapath, header = TRUE, sep = "\t")
+    df <- df %>%
+      rename("X" = "gene")
+    # define the samples to extract 
+    samples <- c(
+      "gene", "H_0001", "H_0002", "H_0003",
+      "H_0005", "H_0006", "H_0007",
+      "H_0008", "H_0009", "H_0010",
+      "H_0012", "H_0013", "H_0539",
+      "H_0657", "H_0658", "H_0681",
+      "H_0695", "H_0700", "H_0726",
+      "H_0740", "H_0750", "C_0012",
+      "C_0013", "C_0014", "C_0015",
+      "C_0016", "C_0017", "C_0018",
+      "C_0020", "C_0021", "C_0022",
+      "C_0023", "C_0024", "C_0025",
+      "C_0026", "C_0029", "C_0031",
+      "C_0032", "C_0033",
+      "C_0035", "C_0036", "C_0037",
+      "C_0038", "C_0039", "C_0050",
+      "C_0053", "C_0060", "C_0061",
+      "C_0062", "C_0065", "C_0069",
+      "C_0070", "C_0071", "C_0075",
+      "C_0076", "C_0077", "C_0081",
+      "C_0082", "C_0083", "C_0087",
+      "C_0002", "C_0003", "C_0004",
+      "C_0005", "C_0006", "C_0008",
+      "C_0009", "C_0010", "C_0011"
+    )
+    selected_df <- df %>% select(one_of(samples))
+    return(selected_df)
   })
   
   ### Define a function that returns the summary table for each column ###
@@ -90,7 +139,7 @@ server <- function(input, output, session) {
       return(summary_data)
     }
   
-  # define a function that filters the sample data based on input value (H vs C)
+  ### define a function that filters the sample data based on input value (H vs C) ###
   filter_samples <- 
     function(data, keyword) {
     keyword <- toupper(keyword)
@@ -99,28 +148,41 @@ server <- function(input, output, session) {
     return(filtered_data)
     }
   
-  # Function to create histograms for each column (excluding the first one)
+  ### define function to create histograms for each column (excluding the first one) ###
   create_histograms <- function(data) {
-    # Exclude the first column
+    # exclude the first column
     numeric_columns <- names(data)[-1]
-    
-    # List to store histograms
+    # list to store histograms
     histograms_list <- list()
-    
-    # Create a histogram for each numeric column
+    # create a histogram for each numeric column
     for (col in numeric_columns) {
       plot <- ggplot(data, aes(x = !!sym(col))) +
         geom_histogram(binwidth = 1, fill = "skyblue", color = "black", alpha = 0.7) +
         labs(title = col) +
         theme_minimal()
-      
-      # Add the plot to the list
+      # add the plot to the list
       histograms_list[[col]] <- plot
     }
-    
     return(histograms_list)
   }
   
+  ### define function to filter genes based on variance and non-zero samples ###
+  filter_genes <- function(data, var_percentile, min_nonzero_samples) {
+    # remove rows with any missing values
+    data <- na.omit(data)
+    # calculate variance for each gene
+    gene_var <- apply(data[-1], 1, var, na.rm = TRUE)
+    # calculate the percentile threshold based on var_percentile
+    var_threshold <- quantile(gene_var, var_percentile / 100, na.rm = TRUE)
+    # calculate the number of non-zero samples for each gene
+    non_zero_samples <- apply(data[-1] != 0, 1, sum, na.rm = TRUE)
+    # filter genes based on criteria
+    selected_genes <- which(gene_var >= var_threshold & non_zero_samples >= min_nonzero_samples)
+    # return the filtered data
+    return(data[selected_genes, , drop = FALSE])
+  }
+  
+  # THESE ARE THE RENDER OUTPUT FUNCTIONS #
   output$sampleTable <- renderTable({
     dataf <- load_sample_data()
     summary_table(dataf)
@@ -162,6 +224,12 @@ server <- function(input, output, session) {
       return(histograms[10])
     }
   })
+  
+  output$countsFilteredTable <- renderTable({
+    dataf <- load_count_data()
+    filter_genes(dataf, input$varianceSlider, input$nonzeroSlider)
+  })
+  
 }
 # Run the application
 shinyApp(ui = ui, server = server)
